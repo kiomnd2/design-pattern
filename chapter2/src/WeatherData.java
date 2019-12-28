c3fe955977f616a303748652f6a40fa035a338a5
kiomnd2 <kiomnd2@gmail.com>
2019-12-28 13:07:36 +0900

    Chapter2. observer Pattern 무엇이 잘못되었나

---
+++ chapter2/src/WeatherData.java
@@ -0,0 +1,34 @@
+public class WeatherData {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    /**
+     * 가상 관측값이
+     * 갱신 될 때마다
+     * 알려주기 위한 메서드
+     */
+    public void measurementsChanged()
+    {
+        float temp = getTemperature();
+        float humidity = getHumidity();
+        float pressure = getPressure();
+
+        currentConditionDisplay.update(temp, humidity, pressure);
+        statisticsDisplay.update(temp, humidity, pressure);
+        forecastDisplay.update(temp, humidity, pressure);
+
+    }
+}
