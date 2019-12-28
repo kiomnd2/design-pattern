468bb6051b9911002ddd3a99d53c4764b3f3f2e1
kiomnd2 <kiomnd2@gmail.com>
2019-12-28 15:35:49 +0900

    Chapter2. 이미지 업로드..

---
+++ chapter1/src/MallardDuck.java
@@ -0,0 +1,12 @@
+public class MallardDuck extends Duck {
+
+
+    public MallardDuck(FlyBehavior flyBehavior, QuackBehavior quackBehavior) {
+        super(flyBehavior, quackBehavior);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("물오리입니다.");
+    }
+}
